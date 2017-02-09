4c5eef5d9b820536b1f5a88493977c64385be8f9
Ozdol, Fethi <fethi.ozdol@accenture.com>
2017-02-09 22:14:59 +0000

    added sonarProjectKey variable

--- adop/jenkins/jobs/dsl/alxeia_pipeline_jobs.groovy
+++ adop/jenkins/jobs/dsl/alxeia_pipeline_jobs.groovy
@@ -1,6 +1,7 @@
 // Folders
 def workspaceFolderName = "${WORKSPACE_NAME}"
 def projectFolderName = "${PROJECT_NAME}"
+def sonarProjectKey = projectFolderName.toLowerCase().replace("/", "-");
 
 // Variables
 def referenceAppGitRepo = "alexia"
